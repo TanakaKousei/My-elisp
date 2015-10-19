631b77717f6553de4baafbe087ff1ba25fe69701
TanakaKousei <13ec062@ms.dendai.ac.jp>
2015-10-19 13:33:52 +0900

    自分用robocodeをemacsでコンパイルするだけのelisp

--- robocode-compile.el
+++ robocode-compile.el
@@ -2,10 +2,9 @@
 
 (defun robocode-compile()
   (interactive)
-  (message "ls")
   (setq filePath (buffer-name))
   (shell-command
-   (format "javac -classpath /usr/lib/jvm/java-8-oracle/jre/lib/rt.jar:/home/User/robocode/libs/robocode.jar:/home/User/robocode/robots %s" (shell-quote-argument (expand-file-name filePath)))
+   (format "javac -classpath /usr/lib/jvm/java-8-oracle/jre/lib/rt.jar:/home/USER/robocode/libs/robocode.jar:/home/USER/robocode/robots %s" (shell-quote-argument (expand-file-name filePath)))
    "*Robo-compile*"))
 (provide 'robocode-compile)
 
